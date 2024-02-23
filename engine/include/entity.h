bbb3932da84fe0fb977cbfc5debb94c2988cc6cc
v01d3d <37172070+lindorffs@users.noreply.github.com>
2024-02-23 14:01:04 -0700

    Create entity.h

---
+++ engine/include/entity.h
@@ -0,0 +1,52 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <SDL.h>
+#include <engine.h>
+#include <chrono>
+#include <state.h>
+#include <string.h>
+
+class OpenSpaceState;
+
+class entity {
+	public:
+		virtual void _internal_render(void);
+		virtual void _internal_update(std::chrono::milliseconds time_delta);
+		SDL_Texture *entity_image = NULL;
+		
+		engine *engine_target = NULL;
+		OpenSpaceState *state_target = NULL;
+		double x;
+		double y;
+		double vx;
+		double vy;
+		double rotation = 0.0;
+		entity();
+		entity(std::string fileName, int x, int y, engine *engine_target, OpenSpaceState *state_target);
+		void render(void);
+		void update(std::chrono::milliseconds time_delta);
+	
+};
+
+class ProjectileEntity : public entity{
+	public:
+		virtual void _internal_render(void);
+		virtual void _internal_update(std::chrono::milliseconds time_delta);
+		ProjectileEntity();
+		ProjectileEntity(std::string fileName, int x, int y, engine *engine_target, OpenSpaceState *state_target, int id);
+		void render(void);
+		void update(std::chrono::milliseconds time_delta);
+		int id;
+};
+
+class PlayerClass : public entity {
+	public:
+		void _internal_render(void);
+		void _internal_update(std::chrono::milliseconds time_delta);
+		PlayerClass(engine *engine_target, OpenSpaceState *state_target);
+		PlayerClass() {};
+		void fire(void);
+};
+
+#endif
